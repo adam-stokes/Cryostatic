9b56550d48a2a5458fce80291aa5012f395702de
Adam Stokes <adam.stokes@ubuntu.com>
2013-12-29 12:55:46 -0500

    update to dzil and not milla
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- cpanfile
+++
@@ -1,9 +0,0 @@
-requires 'perl', '5.008005';
-
-# requires 'Some::Module', 'VERSION';
-requires 'Text::MultiMarkdown', '';
-requires 'Text::FrontMatter::YAML', '';
-
-on test => sub {
-    requires 'Test::More', '0.88';
-};
--- lib/Cryostatic/Rfile.pm
+++ lib/Cryostatic/Rfile.pm
@@ -1,24 +1,29 @@
 package Cryostatic::Rfile;
 
 use Mojo::Base 'Mojolicious::Controller';
-use Mojo::Log;
 use File::Basename;
 use File::Spec::Functions 'catfile';
-use DDP;
 
 sub index {
     my $self = shift;
     my $page_id = $self->param('filename');
     my $abs_path;
-    my $log = Mojo::Log->new;
 
     my ($name, $path, $suffix) = fileparse($page_id, qr/\.[^.]*/);
     $suffix =~ s/^.//s;
     if (exists($self->app->static_ext->{$suffix})) {
-      $abs_path = catfile($self->app->config->{'root_dir'}, $page_id);
-      $log->debug('Found markdown file(' .$abs_path. '), proceeding.');
+        $abs_path = catfile($self->app->config->{'root_dir'}, $page_id);
+        $self->app->log->debug(
+            'Found markdown file(' . $abs_path . '), proceeding.');
     }
-    $self->render(json => {'page_id' => $page_id});
+    $self->respond_to(
+        json => {json => {page_id => $page_id}},
+        html => sub {
+            $self->render(text => 'sup bitches');
+        },
+        any => {text => 'nfc', status => 204}
+    );
+
 }
 
 1;
